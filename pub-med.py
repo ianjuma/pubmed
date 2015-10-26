fd045afc2696b70bffe0226fd40bbac1e29454f7
Ian Juma <ian.juma@yahoo.com>
2015-10-27 00:05:57 +0300

    - updated requirements and updated cite factor scraper per title

--- pub-med.py
+++ pub-med.py
@@ -1,47 +1,15 @@
-
-"""
-import requests
-
-# parsing cite factor
-# from lxml import etree
-# import StringIO
-# from lxml.html import parse
-
-
-from lxml import html
-
-
-doc = parse('http://www.ncbi.nlm.nih.gov/pubmed/?term=cancer').getroot()
-
-for link in doc.cssselect('div.rprt'):
-    print '%s: %s' % (link.text_content(), link.get('href'))
-
-
-# parser = etree.HTMLParser()
-# tree = etree.parse(StringIO.StringIO(page.content), parser)
-
-
-page = requests.get('http://www.ncbi.nlm.nih.gov/pubmed/?term=cancer')
-
-parsed_body = html.fromstring(page.content)
-print parsed_body.xpath('//title/text()')[0].split(' - ')[0]  # search query
-
-print parsed_body.xpath("//div[contains(@class, 'rprt')]/text()")
-
-# print parsed_body.cssselect("div[class='header']")[0].text
-"""
-
 import urllib2
 
 wiki = "http://www.citefactor.org/journal-impact-factor-list-2014_T.html"
 
 page = urllib2.urlopen(wiki)
 from bs4 import BeautifulSoup
-
+import pandas as pd
 
 soup = BeautifulSoup(page)
 
-tables_ = soup.find_all('table', { "class": ''})
+tables_ = soup.find_all('table', {"class": ''})
+data_tables = tables_[1]
 
 A = []
 B = []
@@ -53,16 +21,30 @@ G = []
 H = []
 I = []
 
-for row in soup.find_all('tr'):
-	cells = row.findAll('td')
-	states = row.findAll('th')
-	if len(cells) == 10:
-		A.append(cells[0].find(text=True))
-        B.append(states[0].find(text=True))
-        C.append(cells[1].find(text=True))
-        D.append(cells[2].find(text=True))
-        E.append(cells[3].find(text=True))
-        F.append(cells[4].find(text=True))
-        G.append(cells[5].find(text=True))
-        H.append(cells[6].find(text=True))
-        I.append(cells[7].find(text=True))
+
+for row in data_tables.find_all('tr'):
+    cells = row.findAll('td')
+    # states = row.findAll('th')
+    if len(cells) == 9:
+        A.append(cells[0].find(text=True))
+        B.append(cells[1].find(text=True))
+        C.append(cells[2].find(text=True))
+        D.append(cells[3].find(text=True))
+        E.append(cells[4].find(text=True))
+        F.append(cells[5].find(text=True))
+        G.append(cells[6].find(text=True))
+        H.append(cells[7].find(text=True))
+        I.append(cells[8].find(text=True))
+
+
+df = pd.DataFrame(A, columns=['Index'])
+df['Journal'] = B
+df['ISSN'] = C
+df['2013/2014'] = D
+df['2012'] = E
+df['2011'] = F
+df['2010'] = G
+df['2009'] = H
+df['2008'] = I
+
+print df
