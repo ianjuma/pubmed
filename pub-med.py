42a1538dad9b50518cb7f0241f3773f84e5f9dfb
Ian Juma <ian.juma@yahoo.com>
2015-10-26 16:24:24 +0300

    - updated requirements / dependencies

--- pub-med.py
+++ pub-med.py
@@ -1,3 +1,5 @@
+
+"""
 import requests
 
 # parsing cite factor
@@ -8,7 +10,7 @@ import requests
 
 from lxml import html
 
-"""
+
 doc = parse('http://www.ncbi.nlm.nih.gov/pubmed/?term=cancer').getroot()
 
 for link in doc.cssselect('div.rprt'):
@@ -17,7 +19,7 @@ for link in doc.cssselect('div.rprt'):
 
 # parser = etree.HTMLParser()
 # tree = etree.parse(StringIO.StringIO(page.content), parser)
-"""
+
 
 page = requests.get('http://www.ncbi.nlm.nih.gov/pubmed/?term=cancer')
 
@@ -27,3 +29,40 @@ print parsed_body.xpath('//title/text()')[0].split(' - ')[0]  # search query
 print parsed_body.xpath("//div[contains(@class, 'rprt')]/text()")
 
 # print parsed_body.cssselect("div[class='header']")[0].text
+"""
+
+import urllib2
+
+wiki = "http://www.citefactor.org/journal-impact-factor-list-2014_T.html"
+
+page = urllib2.urlopen(wiki)
+from bs4 import BeautifulSoup
+
+
+soup = BeautifulSoup(page)
+
+tables_ = soup.find_all('table', { "class": ''})
+
+A = []
+B = []
+C = []
+D = []
+E = []
+F = []
+G = []
+H = []
+I = []
+
+for row in soup.find_all('tr'):
+	cells = row.findAll('td')
+	states = row.findAll('th')
+	if len(cells) == 10:
+		A.append(cells[0].find(text=True))
+        B.append(states[0].find(text=True))
+        C.append(cells[1].find(text=True))
+        D.append(cells[2].find(text=True))
+        E.append(cells[3].find(text=True))
+        F.append(cells[4].find(text=True))
+        G.append(cells[5].find(text=True))
+        H.append(cells[6].find(text=True))
+        I.append(cells[7].find(text=True))
