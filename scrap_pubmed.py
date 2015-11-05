9a71e61586842e9366dd5055b0b0db6835a1608e
Ian Juma <ian.juma@yahoo.com>
2015-11-05 14:08:58 +0300

    - fixed ascii code issue

--- scrap_pubmed.py
+++ scrap_pubmed.py
@@ -6,6 +6,7 @@ from pub_med import fetch_page_per_index, retrieve_impact_factor
 email = 'wjuma@students.usiu.ac.ke'
 target_encoding = "utf-8"
 
+
 def search(query, number_of_articles_):
     Entrez.email = email
     handle = Entrez.esearch(db='pubmed',
@@ -44,15 +45,18 @@ if __name__ == '__main__':
         target.write('Article ID: ' + paper['MedlineCitation']['PMID'] + '\n')
 
         print 'ISSN No: ', paper['MedlineCitation']['Article']['Journal']['ISSN']
-        target.write('ISSN No: ' + paper['MedlineCitation']['Article']['Journal']['ISSN'] + '\n')
+        target.write('ISSN No: ' + paper['MedlineCitation'][
+                     'Article']['Journal']['ISSN'] + '\n')
 
         try:
-            year = paper['MedlineCitation']['Article']['ArticleDate'][0]['Year']
+            year = paper['MedlineCitation']['Article'][
+                'ArticleDate'][0]['Year']
         except (IndexError, KeyError):
             year = None
 
         try:
-            month = paper['MedlineCitation']['Article']['ArticleDate'][0]['Month']
+            month = paper['MedlineCitation']['Article'][
+                'ArticleDate'][0]['Month']
         except (IndexError, KeyError):
             month = None
 
@@ -67,37 +71,55 @@ if __name__ == '__main__':
         target.write('Date published: %s-%s-%s \n' % (year, month, day) + '\n')
 
         # get journal impact factor
-        data_frame = fetch_page_per_index(paper['MedlineCitation']['Article']['ArticleTitle'])
+        data_frame = fetch_page_per_index(
+            paper['MedlineCitation']['Article']['ArticleTitle'])
         impact_factor = retrieve_impact_factor(paper['MedlineCitation']['Article']['Journal']['ISSN'], data_frame)
         print "Impact factor - " + impact_factor + "\n"
         target.write("Impact factor - " + impact_factor + "\n")
 
         print paper['MedlineCitation']['Article']['ArticleTitle'] + "\n"
-        target.write(paper['MedlineCitation']['Article']['ArticleTitle'].encode('utf-8') + '\n')
+        target.write(paper['MedlineCitation']['Article'][
+                     'ArticleTitle'].encode('utf-8') + '\n')
         print paper['MedlineCitation']['Article']['Abstract']['AbstractText'][0]
-        target.write(str(paper['MedlineCitation']['Article']['Abstract']['AbstractText']).encode('utf-8') + '\n')
+        target.write(str(paper['MedlineCitation']['Article'][
+                     'Abstract']['AbstractText']).encode('utf-8') + '\n')
 
         print "\n---------------------- AUTHORS ------------------------ \n"
-        target.write("\n---------------------- AUTHORS ------------------------ \n")
+        target.write(
+            "\n---------------------- AUTHORS ------------------------ \n")
         for author in paper['MedlineCitation']['Article']['AuthorList']:
             number_of_authors += 1
             try:
-                last_name = author['LastName']
+                last_name = author.get('LastName', None)
             except IndexError:
                 last_name = None
 
             try:
-                fore_name = author['ForeName']
+                fore_name = author.get('ForeName', None)
             except IndexError:
                 fore_name = None
 
             try:
-                author_affiliation = author['AffiliationInfo'][0]['Affiliation']
+                author_affiliation = author[
+                    'AffiliationInfo'][0]['Affiliation']
             except IndexError:
                 author_affiliation = None
 
+            try:
+                last_name = unicode(last_name, 'latin-1')
+            except UnicodeDecodeError:
+                pass
+
+            try:
+                fore_name = unicode(fore_name, 'latin-1')
+            except UnicodeDecodeError:
+                pass
+
+            author_affiliation = unicode(author_affiliation, 'latin-1')
+
             print last_name, fore_name, "--", author_affiliation
-            target.write(str(last_name).encode('utf-8') + str(fore_name).encode('utf-8') + "->" + str(author_affiliation).encode('utf-8') + '\n')
+            target.write(str(last_name) + str(
+                fore_name) + "->" + str(author_affiliation) + '\n')
 
         print "total author # - %d authors" % number_of_authors
         target.write("total author # - %d authors" % number_of_authors + '\n')
